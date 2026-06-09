92dcea66b80bf106b9ddc7c09e4c67930f39d668
c0c250869a <c0c250869a@edu.teu.ac.jp>
2026-06-09 16:24:30 +0900

    練習問題終わり

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,29 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)  # 練習8：背景画像の左右反転
+    kk_img = pg.image.load("fig/3.png")  # 練習3：こうかとんSurfaceの作成
+    kk_img = pg.transform.flip(kk_img, True, False)  # 練習3：こうかとんの左右
+    kk_rct = kk_img.get_rect() #練習10
+    kk_rct.center = 300, 200#練習10-2:こうかとん
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
+        key_lst = pg.key.get_pressed() #練習10-3
+        #print(key_let)
+        if key_lst[pg.k_UP]:
+            kk_rct.move_ip(0, -1) 
+
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0])  # 練習5：背景画像を右から左へ
+        screen.blit(bg_img2, [-x+1600, 0])  # 練習7：2枚目Surface
+        screen.blit(bg_img, [-x+3200, 0])  # 練習9：3枚目の背景画像の描画
+        screen.blit(kk_img, kk_rct)  # 練習4：こうかとんSurfaceの描画
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(400)
 
 
 if __name__ == "__main__":
